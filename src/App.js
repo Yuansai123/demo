df22d9a4054ae7ef8dae34d29157f3e9950ab987
Mac <475013647@qq.com>
2020-04-06 12:47:47 +0800

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      管理平台
+    </div>
+  );
+}
+
+export default App;
